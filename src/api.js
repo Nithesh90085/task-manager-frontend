418857acf37d1339ced9e487b3ea924973aa4e64
Nithesh90085 <nithesh325@gmail.com>
2025-09-07 17:05:50 +0530

    Update API URL to live backend

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000", // backend base URL
+  baseURL: "https://task-manager-backend-c9lp.onrender.com", // backend base URL
 });
 
 // Get all tasks
